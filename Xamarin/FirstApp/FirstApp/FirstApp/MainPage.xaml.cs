80ec4e3f2bf8a08ba51f9ef62821a00d538fe804
faisalcse1 <faisalcse1@gmail.com>
2021-07-03 10:09:30 +0600

    xamarin first app...

---
+++ Xamarin/FirstApp/FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        //Label labelInfo = null;
+        //Entry Name = null;
+        //Button ShowButton = null;
+        //Label lblName = null;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //labelInfo = new Label() { Text = "Name" };
+            //Name = new Entry() { Placeholder = "Enter your name" };
+            //ShowButton = new Button() { Text = "Show", TextColor = Color.White, BackgroundColor = Color.Blue };
+            //ShowButton.Clicked += ShowButton_Clicked;
+            //lblName = new Label() { HorizontalOptions=LayoutOptions.Center,
+            //VerticalOptions=LayoutOptions.Center};
+
+            //this.Content = new StackLayout
+            //{
+            //    Children =
+            //    {
+            //        labelInfo,
+            //        Name,
+            //        ShowButton,
+            //        lblName
+            //    }
+            //};
+
+            //LabelInfo.Text = "Hello world";           
+        }
+
+        private void ShowButton_Clicked(object sender, EventArgs e)
+        {
+            string name = Name.Text;
+            Name.Text = String.Empty;
+            lblName.Text = name;
+
+        }
+    }
+}
